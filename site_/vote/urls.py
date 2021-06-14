6a4bda2eca5e0559e84a4f52d4c9bf3b87d5d0e2
alireza <alireza.daghigh1999@gmail.com>
2021-06-15 02:27:52 +0430

    end

--- site_/vote/urls.py
+++ site_/vote/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from . views import index_vote
+from . views import index_vote, all_questions, show_detail
+app_name = "vote"
 urlpatterns = [
-    path('', index_vote )
+    path('', index_vote, name='index' ),
+    path('questions/', all_questions, name='all' ),
+    path('questions/<int:question_id>/', show_detail, name='detail'),
+
 ]
--- site_/vote/views.py
+++ site_/vote/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, get_list_or_404, get_object_or_404
 # Create your views here.
-
+from . models import Question, Choice
 def index_vote(request, *args, **kwargs):
     return HttpResponse("hello")
+
+
+def all_questions(request, *args, **kwargs):
+
+    all_questions = Question.objects.all()
+    context = {
+        "all_q": all_questions
+    }
+    return render(request, 'show_all.html', context )
+
+def show_detail(request, question_id):
+    if request.method == "GET":
+        q = Question.objects.get(id=question_id)
+        context = {
+            "question":q
+        }
+        return render(request, 'show_detail.html',context)
+    
+    else:
+        q = Question.objects.get(id=question_id)
+
+        try:
+            choice_selected = q.choice_set.get(id = request.POST["choice"])
+        except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+            return render(request, 'show_detail.html', {
+                'question': q,
+                'error_message': "You didn't select a choice.",
+            })
+        else:
+            choice_selected.vote += 1
+            choice_selected.save()
+        return HttpResponse("submited")
