aed09c3913d0cb0438d87c455d9ba46af799977b
alireza <alireza.daghigh1999@gmail.com>
2021-06-15 18:30:49 +0430

    finish proj

--- site_/vote/urls.py
+++ site_/vote/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . views import index_vote, all_questions, show_detail
+from . views import index_vote,ShowAll, show_detail, ShowDetail
 app_name = "vote"
 urlpatterns = [
     path('', index_vote, name='index' ),
-    path('questions/', all_questions, name='all' ),
+    path('questions/', ShowAll.as_view(), name='all' ),
+    path('questions/vote/<pk>',ShowDetail.as_view(),name="show_detail" ),
     path('questions/<int:question_id>/', show_detail, name='detail'),
 
 ]
--- site_/vote/views.py
+++ site_/vote/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse, get_list_or_404, get_object_or_404
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 # Create your views here.
 from . models import Question, Choice
 def index_vote(request, *args, **kwargs):
     return HttpResponse("hello")
 
+class ShowAll(ListView):
+    model = Question
+    context_object_name="all_q"
+    template_name = "show_all.html"
 
-def all_questions(request, *args, **kwargs):
+# def all_questions(request, *args, **kwargs):
 
-    all_questions = Question.objects.all()
-    context = {
-        "all_q": all_questions
-    }
-    return render(request, 'show_all.html', context )
+#     all_questions = Question.objects.all()
+#     context = {
+#         "all_q": all_questions
+#     }
+#     return render(request, 'show_all.html', context )
+
+class ShowDetail(DetailView):
+    template_name = 'show_detail.html'
+    context_object_name = "question"
+    model = Question
+    
+        
+
 
 def show_detail(request, question_id):
     if request.method == "GET":
